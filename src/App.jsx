5cb449937dba0b718f09cce1921cc959ed5046bf
theredwillow <theredwillows@gmail.com>
2019-12-30 23:09:27 -0600

    Moved background lines into individual div's

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,29 @@ const App = () => (
   <div className="App">
 
     <div id="red" className="section">
-        <div>
-          Front-End Developer / UX Developer / JavaScript Expert
-        </div>
-        <div>
-          From Dallas / Fort Worth - DFW - Funky Town and Big D
-        </div>
-        <div>
-          Son of a webmaster. Learned programming at a young age. Long-time programming hobbyist. Weekend warrior.
-        </div>
+      About Me
+      <div className="median">
+        <div className="red line"></div>
+        <div className="green line"></div>
+        <div className="blue line"></div>
+      </div>
+      <div>
+        Front-End Developer / UX Developer / JavaScript Expert
+      </div>
+      <div>
+        From Dallas / Fort Worth - DFW - Funky Town and Big D
+      </div>
+      <div>
+        Son of a webmaster. Learned programming at a young age. Long-time programming hobbyist. Weekend warrior.
+      </div>
     </div>
 
     <div id="green" className="section">
+      <div className="median">
+        <div className="red line"></div>
+        <div className="green line"></div>
+        <div className="blue line"></div>
+      </div>
         <div>
           <a
             href="https://www.weidewedding2019.com/"
@@ -47,7 +58,12 @@ const App = () => (
     </div>
 
     <div id="blue" className="section">
-      <div>
+      <div className="median">
+        <div className="red line"></div>
+        <div className="green line"></div>
+        <div className="blue line"></div>
+      </div>
+      <div className="neon">
         August 2019 - December 2019
         Cottonwood Financial
       </div>
@@ -56,27 +72,6 @@ const App = () => (
         September 2017 - August 2019
         Rental Beast
       </div>
-    </div>
-
-    <div id="median">
-      <div className="red line selected"></div>
-      <div className="green line"></div>
-      <div className="blue line"></div>
-    </div>
-
-    <div id="control-panel">
-      <div>Jared Weide</div>
-
-      <div className="jump">
-        <div className="red button selected">About Me</div>
-      </div>
-      <div className="jump">
-        <div className="green button">Projects</div>
-      </div>
-      <div className="jump">
-        <div className="blue button">Experience</div>
-      </div>
-      
     </div>
 
   </div>
